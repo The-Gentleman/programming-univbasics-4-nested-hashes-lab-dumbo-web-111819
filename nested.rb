22f284968eb105451a1942d30f14660ed45abb4f
Zeshan Raja <zraja4402@gmail.com>
2019-11-08 21:59:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,10 +64,9 @@ def adding_matz
 #   :known_for => "Ruby",
 #     :languages => ["LISP", "C"]
 # }	
-
-	programmer_hash = 
- 	programmer_hash[:yukihiro_matsumoto][:known_for] = 'Ruby', 
+	programmer_hash[:yukihiro_matsumoto][:known_for] = 'Ruby', 
   programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+	programmer_hash = 
  			{
         :grace_hopper => {
           :known_for => "COBOL",
